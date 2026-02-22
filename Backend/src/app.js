a4c302e06557538b457018e33a80b015652b97db
Suyog <155283878+cosmicsuyog@users.noreply.github.com>
2026-02-22 19:09:04 +0530

    Update Repo

---
+++ Backend/src/app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const noteModel = require("./models/note.model");
+const path = require("path");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("./public"));
+
+app.post("/api/notes", async (req, res) => {
+  const { title, description } = req.body;
+  const note = await noteModel.create({
+    title,
+    description,
+  });
+  res.status(201).json({
+    message: "note created successfully",
+    note,
+  });
+});
+
+app.get("/api/notes", async (req, res) => {
+  const notes = await noteModel.find();
+  res.status(200).json({
+    message: "Data fetched Successfully",
+    notes,
+  });
+});
+
+app.delete("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await noteModel.findByIdAndDelete(id);
+  res.status(200).json({
+    message: "Note deleted successfully",
+  });
+});
+
+app.patch("/api/notes/:id", async (req, res) => {
+  const id = req.params.id;
+  const { description } = req.body;
+
+  await noteModel.findByIdAndUpdate(id, { description });
+  res.status(200).json({
+    message: "Note updated successfully",
+  });
+});
+
+module.exports = app;
