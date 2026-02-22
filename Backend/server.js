c39fcd318afbfacd38321e889bc518c61ab662ca
Suyog <155283878+cosmicsuyog@users.noreply.github.com>
2026-02-22 19:30:29 +0530

    Updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,11 @@
 require("dotenv").config();
 const app = require("./src/app");
-const connectToDB =require("./src/config/database")
+const connectToDB = require("./src/config/database");
 
+connectToDB();
 
-connectToDB()
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, async (req, res) => {
-  console.log("server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
